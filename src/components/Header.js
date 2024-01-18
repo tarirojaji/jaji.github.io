7f50c437c9328bb91ec536240c2ea751405f07f5
tarirojaji <57304962+tarirojaji@users.noreply.github.com>
2024-01-18 12:54:02 +0000

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,11 @@ const Header = () => {
         <table className='Header'>
             <tbody>
                 <tr className='topHeaderRow'>
-                    <td className='name'>Tariro <b>Kagogoda</b></td>
+                    <td className='name'>Tariro Jaji <b>Kagogoda</b></td>
                     
                     <td className='contact'>
-                        <p> <box-icon name='envelope' /> tarirojiji@gmail.com <br /></p>
+                        <p> <box-icon name='envelope' /> tarirojkg@gmail.com <br /></p>
                         <p> <box-icon type='logo' name='github' /> github.com/tarirojaji <br /></p>
-                        <p> <box-icon name='hash' /> tarirojiji@gmail.com <br /></p>
                         <p> <box-icon name='location-plus' /> Portsmouth, UK <br /></p>
                     </td>
                 </tr>
